df96944bceb8e7ea827039ef1d20b6abdee9aa0d
codejvn <65090247+codejvn@users.noreply.github.com>
2023-02-19 16:54:13 -0500

    working python

--- data.py
+++ data.py
@@ -1,8 +1,15 @@
 import requests
 import json
+import openai
 import geopy
 from geopy.geocoders import Nominatim
 
+user_city = input("City, State >>> ")
+file = open("info.txt", "w")
+file.write(user_city)
+file.close()
+
+#City Antipode Finder
 geolocator = Nominatim(user_agent="Better Weather")
 
 
@@ -13,14 +20,20 @@ def readCity():
     return city
 
 def describeWeather(windSpeed, pressure, precip, cloudCover, temp, humidity):
-    windy = False#
-    rainy = 0#
-    snowy = 0#
-    stormy = False#
-    cloudy = 0#
-    foggy = False#
-    weathers = [windy, rainy, snowy, stormy, cloudy, foggy]
+    windy = False
+    rainy = 0
+    snowy = 0
+    stormy = False
+    cloudy = 0
+    foggy = False
+    sunny = True
+    weathers = [windy, rainy, snowy, stormy, cloudy, foggy, sunny]
+    strings = ['windy', 'rainy', 'snowy', 'stormy', 'cloudy', 'foggy', 'sunny']
     forecast = []
+
+    file = open("info.txt", "w")
+    file.write(str(temp))
+    file.close()
 
     if windSpeed > 24: windy = True
     if precip < 0.1: rainy = 0
@@ -39,29 +52,44 @@ def describeWeather(windSpeed, pressure, precip, cloudCover, temp, humidity):
     elif cloudCover < 70: cloudy = 2
     else: cloudy = 3
     if (humidity > 95) and (pressure > 31) and (windSpeed < 10) and (cloudCover > 70): foggy = True
+    if cloudy != 0: sunny = False
 
     for weatherType in weathers:
         if weatherType == True:
-            forecast.append(weatherType)
+            forecast.append(strings[weathers.index(weatherType)])
         elif weatherType == 1:
-            forecast.append("slightly " + weatherType)
+            forecast.append("slightly " + strings[weathers.index(weatherType)])
         elif weatherType == 2:
-            forecast.append(weatherType)
+            forecast.append(strings[weathers.index(weatherType)])
         elif weatherType == 3:
-            forecast.append("heavily " + weatherType)
+            forecast.append("heavily " + strings[weathers.index(weatherType)])
     return forecast
 
 def getAntipode(city):
     location = geolocator.geocode(city)
-    latO, lonO = location.latitude(), location.longitude()
+    latO = str(location.latitude)
+    lonO = str(location.longitude)
+    latO, lonO = eval(latO), eval(lonO)
     latA = latO * -1
     lonA = lonO + 180
     return latA, lonA
 
+#Weather of Antipode
 lat, lon = getAntipode(readCity())
-data = requests.get(f"http://api.weatherstack.com/current? access_key = d0a2334b8769a13ce02f5eef1b7c418e& query = {lat,lon}")
-data = json.dumps(data, set_keys=True, indent=4)
-forecast = describeWeather(data[2][5], data[2][8], data[2][9], data[2][11], data[2][1], data[2][10])
+key = "90724d418f86432b979195649231902"
+print(f"http://api.weatherapi.com/v1/current.json?key={key}&q={lat},{lon}")
+data = requests.get(f"http://api.weatherapi.com/v1/current.json?key={key}&q={lat},{lon}")
+data = json.dumps(data.json(), sort_keys=True, indent=4)
+data = json.loads(data)
+forecast = describeWeather(
+    int(data['current']['wind_mph']),
+    int(data['current']['pressure_in']),
+    int(data['current']['precip_mm']), 
+    int(data['current']['cloud']), 
+    int(data['current']['temp_f']), 
+    int(data['current']['humidity'])
+)
+
 """Order: windSpeed, pressure, precip, cloudCover, temp, humidity
 5 windSpeed
 8 pressure
@@ -70,16 +98,19 @@ forecast = describeWeather(data[2][5], data[2][8], data[2][9], data[2][11], data
 11 cloud cover
 1 feels like"""
 
-import openai
-
-openai.api_key = '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'
+#AI pickup lines
+openai.api_key = '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'
 
 prompt_weather = ""
-for weather in forecast: prompt_weather += (weather + ", ")
+for weather in forecast: prompt_weather += (weather + " ")
+
+file = open("info.txt", "a")
+file.write("\nToday is a " + prompt_weather + " day!")
+file.close()
 
 response = openai.Completion.create(
   model="text-davinci-003",
-  prompt=f"Write a pickup line for a {prompt_weather} day relating to that weather.",
+  prompt=f"Write a hot pickup line for a {prompt_weather} day relating to that weather.",
   temperature=0.7,
   max_tokens=256,
   top_p=1,
@@ -87,6 +118,6 @@ response = openai.Completion.create(
   presence_penalty=0
 )
 
-file = open("info.txt", "w")
-file.write(response)
+file = open("info.txt", "a")
+file.write(response['choices'][0]['text'])
 file.close()
